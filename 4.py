fe0aa96437a58a377c263c1a4200dffa54fd4db9
Explore <bogdan05gel@gmail.com>
2025-05-16 03:19:32 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -11,24 +11,3 @@ class Car:
 c = Car("Toyota", "Corolla")
 print(c.make)  # Toyota
 print(c.color)  # This attribute is not available
-
-#4
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def __setattr__(self, key, value):
-        if key not in self.__dict__:
-            raise AttributeError("Local attributes are not allowed")
-        super().__setattr__(key, value)
-
-# Пример использования:
-r = Rectangle(10, 20)
-r.width = 15  # Успешно
-r.height = 25  # Успешно
-
-try:
-    r.color = 'red'  # AttributeError: Local attributes are not allowed
-except AttributeError as e:
-    print(e)
